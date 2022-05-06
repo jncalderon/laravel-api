26b3b22114c1ca672284d363670301b8f133d0aa
Nelson Calderón <jncalderon.b@gmail.com>
2022-05-05 18:06:23 -0600

    more coverage tests, added helpers, fix in helper calls

---
+++ tests/Feature/FeatureExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Jncalderon\LaravelApi\Tests\Feature;
+
+use Jncalderon\LaravelApi\Tests\TestCase;
+
+class FeatureExample extends TestCase
+{
+    /** @test */
+    public function example_test()
+    {
+        $this->assertTrue(true);
+    }
+}
